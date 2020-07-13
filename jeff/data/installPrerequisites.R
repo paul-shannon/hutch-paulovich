b4bbe85dca5ed2e2090f4a203128bafcac9d898d
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-07-13 05:59:41 -0700

    forgot this on previous commit

---
+++ jeff/data/installPrerequisites.R
@@ -0,0 +1,30 @@
+printf <- function(...) print(noquote(sprintf(...)))
+install.packages("BiocManager", repos="https://cran.rstudio.com")
+biocGet <- function(pkgs){
+   library(BiocManager)
+   BiocManager::install(pkgs, update=FALSE)
+   }
+
+
+code.pkgs <- c("shinydashboard",
+               "DT",
+               "shinyjs",
+               "shinyBS",
+               "later",
+               "ggplot2",
+               "VariantAnnotation",
+               "org.Hs.eg.db"
+               )
+
+for(code.pkg in code.pkgs){
+   suppressWarnings(
+      needed <- !require(code.pkg, character.only=TRUE, lib.loc=my.user.library, quiet=TRUE)
+      )
+   printf("%s needed? %s", code.pkg, needed)
+   if(needed)
+      biocGet(code.pkg)
+   } # for
+
+
+
+
